6c44d1e3e3934af4cf841d4c42b0bed9b89bbcd1
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-04-02 15:39:47 +0530

    socket util update

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -9,6 +9,8 @@ const createSecretRoomId = (roomId: string) => {
 
 export const intializeSocket = (server: any) => {
   const io = new Server(server, {
+    path:
+      process.env.NODE_ENV === "production" ? "/api/socket.io" : "/socket.io",
     cors: {
       origin: [
         "http://localhost:5173",
